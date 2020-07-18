b6c84acf0ab9fbdf9ad8c37da9c6e2af246f22dc
Thomas Van <tinbolw@Thomass-MacBook-Pro.local>
2020-07-18 16:56:18 -0700

    added second one

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 
 done = None
 
-
-
-
 questionsCorrect = 0
 questionsAmount = 0
 question1 = ""
 answer1 = ""
+question2 = ""
+answer2 = ""
 
 def askQuestion2():
 	print("wip")
@@ -26,20 +25,37 @@ def askQuestion1():
 		questionsCorrect += 1
 		questionsCorrect = str(questionsCorrect)
 		questionsAmount = str(questionsAmount)
-		print("Your score is: + " + questionsCorrect + "/" + questionsAmount)
+		print("Your score is: " + questionsCorrect + "/" + questionsAmount)
 		askQuestion2()
 	elif answer != answer1:
 		tryagain = input("Incorrect\nDo you want to try again?\n")
 		tryagain = str(tryagain)
 		if tryagain == "Y" or "y" or "yes" or "Yes":
 			askQuestion1()
-		elif tryagain == "N" or "n" or "no" or "No":
+		elif tryagain == str(N) or "n" or "no" or "No":
 			askQuestion2()
 
 def askDone():
 	global done
 	done = input("Are you done?\n")
 	done = str(done)
+
+def getQuestion2():
+	global answer2
+	global done
+	global questionsAmount
+	global question2
+	question2 = input("What is the question?\n")
+	answer2 = input("What is the answer?\n")
+	questionsAmount += 1
+	askDone()
+	if done == "Y" or "y" or "yes" or "Yes":
+		print("Ok")
+		os.system('cls' if os.name == 'nt' else 'clear')
+		askQuestion2()
+	elif done == "N" or "n" or "no" or "No":
+		done = None
+		getQuestion3()
 
 def getQuestion1():
 	global answer1
@@ -55,6 +71,7 @@ def getQuestion1():
 		os.system('cls' if os.name == 'nt' else 'clear')
 		askQuestion1()
 	elif done == "N" or "n" or "no" or "No":
+		done = None
 		getQuestion2()
 
 getQuestion1()
