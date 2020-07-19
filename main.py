9f03cf898221bc21cd60dd8753f71e4d8f495b60
Thomas Van <tinbolw@Thomass-MacBook-Pro.local>
2020-07-18 22:41:39 -0700

    restarted

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-import os
-
-done = None
 
 questionsCorrect = 0
 questionsAmount = 0
 question1 = ""
 answer1 = ""
-question2 = ""
-answer2 = ""
-
-def askQuestion2():
-	print("wip")
+isDone = ""
 
 def askQuestion1():
-	global question1
 	global questionsCorrect
-	global questionsAmount
+	global question1
 	global answer1
 	print(question1)
 	answer = input("What is the answer?\n")
@@ -23,55 +15,34 @@ def askQuestion1():
 	if answer == answer1:
 		print("Correct!")
 		questionsCorrect += 1
-		questionsCorrect = str(questionsCorrect)
-		questionsAmount = str(questionsAmount)
-		print("Your score is: " + questionsCorrect + "/" + questionsAmount)
 		askQuestion2()
 	elif answer != answer1:
-		tryagain = input("Incorrect\nDo you want to try again?\n")
+		tryagain = input("Incorrect\n Do you want to try again?\n")
 		tryagain = str(tryagain)
 		if tryagain == "Y" or "y" or "yes" or "Yes":
 			askQuestion1()
-		elif tryagain == str(N) or "n" or "no" or "No":
-			askQuestion2()
+		elif tryagain == "N" or "n" or "no" or "No":
+			askQuestion2
 
-def askDone():
-	global done
-	done = input("Are you done?\n")
-	done = str(done)
-
-def getQuestion2():
-	global answer2
-	global done
-	global questionsAmount
-	global question2
-	question2 = input("What is the question?\n")
-	answer2 = input("What is the answer?\n")
-	questionsAmount += 1
-	askDone()
-	if done == "Y" or "y" or "yes" or "Yes":
-		print("Ok")
-		os.system('cls' if os.name == 'nt' else 'clear')
-		askQuestion2()
-	elif done == "N" or "n" or "no" or "No":
-		done = None
-		getQuestion3()
 
 def getQuestion1():
-	global answer1
-	global done
 	global questionsAmount
 	global question1
+	global answer1
 	question1 = input("What is the question?\n")
 	answer1 = input("What is the answer?\n")
 	questionsAmount += 1
-	askDone()
-	if done == "Y" or "y" or "yes" or "Yes":
-		print("Ok")
-		os.system('cls' if os.name == 'nt' else 'clear')
-		askQuestion1()
-	elif done == "N" or "n" or "no" or "No":
-		done = None
-		getQuestion2()
+	isDone = input("Are you done? (Y/N)\n")
+	isDone = str(isDone)
+	def checkDone1():
+		if isDone == str(Y):
+			print("Ok")
+			askQuestion1()
+		elif isDone == str(N):
+			getQuestion2()
+		else:
+			print("Invalid Selection")
+			checkDone1()
+	checkDone1()
 
 getQuestion1()
