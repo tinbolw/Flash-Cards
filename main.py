466f95984ad34caec2d891fd784c20787ca43044
Thomas Van <tinbolw@Thomass-MacBook-Pro.local>
2020-07-19 22:42:41 -0700

    fixed bug that would occur if you only had one question

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
+import os
 
 continue1 = ""
 question1 = ""
 answer1 = ""
+continue2 = ""
+question2 = ""
+answer2 = ""
 questionsAmount = 0
 questionsCorrect = 0
 
-def getCard2():
-	question2 = input("What is the question?\n")
-	answer2 = input("What is the answer?\n")
-	continue2 = input("Continue? (Y/N)\n")
 
 def askQuestion2():
-	print("WIP")
+	global question2
+	global answer2
+	global questionsAmount
+	global questionsCorrect
+	print(question2)
+	evaluate2 = input("What is the answer?\n")
+	if evaluate2 == answer2:
+		print("Correct!\n")
+		questionsAmount = int(questionsAmount)
+		questionsCorrect = int(questionsCorrect)
+		questionsCorrect += 1
+		questionsAmount = str(questionsAmount)
+		questionsCorrect = str(questionsCorrect)
+		print("Your score is: " + questionsCorrect + "/" + questionsAmount)
+		askQuestion3()
+	elif evaluate2 != answer2:
+		print("Incorrect!")
+		askQuestion2()
 
 
 def askQuestion1():
 	global question1
+	global question2
 	global answer1
 	global questionsAmount
 	global questionsCorrect
@@ -29,16 +47,41 @@ def askQuestion1():
 		questionsAmount = str(questionsAmount)
 		questionsCorrect = str(questionsCorrect)
 		print("Your score is: " + questionsCorrect + "/" + questionsAmount)
+		if question2 == "":
+			quit()
 		askQuestion2()
 	elif evaluate1 != answer1:
 		print("Incorrect!")
+		if question2 == "":
+			quit()
 		askQuestion2()
 
+def funcContinue2():
+	if continue2 == "Y" or continue1 == "y" or continue1 == "yes" or continue1 == "Yes":
+		getCard3()
+	elif continue1 == "N" or continue1 == "n" or continue1 == "no" or continue1 == "No":
+		os.system('cls' if os.name == 'nt' else 'clear')
+		askQuestion1()
+	else:
+		print("Invalid Selection")
+		funcContinue2()
+
+def getCard2():
+	global question2
+	global continue2
+	global answer2
+	global questionsAmount
+	question2 = input("What is the question?\n")
+	answer2 = input("What is the answer?\n")
+	continue2 = input("Continue? (Y/N)\n")
+	questionsAmount += 1
+	funcContinue2()
 
 def funcContinue1():
 	if continue1 == "Y" or continue1 == "y" or continue1 == "yes" or continue1 == "Yes":
 		getCard2()
 	elif continue1 == "N" or continue1 == "n" or continue1 == "no" or continue1 == "No":
+		os.system('cls' if os.name == 'nt' else 'clear')
 		askQuestion1()
 	else:
 		print("Invalid Selection")
